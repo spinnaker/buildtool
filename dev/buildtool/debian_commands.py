a5ed4f9796676c27d1252808ed1ee9d708089570
Michael Plump <plumpy@google.com>
2020-02-25 12:20:23 -0500

    feat(bintray): publish to bintray from GCB

--- dev/buildtool/debian_commands.py
+++ dev/buildtool/debian_commands.py
@@ -19,10 +19,12 @@ from threading import Semaphore
 
 from buildtool import (
     BomSourceCodeManager,
+    BranchSourceCodeManager,
     GradleCommandProcessor,
     GradleCommandFactory,
 
     check_options_set,
+    check_subprocesses_to_logfile,
     raise_and_log_error,
     ConfigError)
 
@@ -56,31 +58,65 @@ class BuildDebianCommand(GradleCommandProcessor):
     options = self.options
     name = repository.name
     args = self.gradle.get_common_args()
-    if options.gradle_cache_path:
-      args.append('--gradle-user-home=' + options.gradle_cache_path)
-
-    if (not options.run_unit_tests
-        or (name == 'deck' and not 'CHROME_BIN' in os.environ)):
-      args.append('-x test')
-
-    args.extend(self.gradle.get_debian_args('trusty,xenial,bionic'))
-
+    cloudbuild_config = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'cloudbuild', 'debs.yml')
+    service_name = self.scm.repository_name_to_service_name(repository.name)
     source_info = self.scm.lookup_source_info(repository)
-    with self.__semaphore:
-      self.gradle.check_run(args, self, repository, 'candidate', 'debian-build',
-                            source_info.summary.version,
-                            source_info.build_number)
+    command = ('gcloud builds submit '
+               ' --account={account} --project={project}'
+               ' --substitutions='
+               '_IMAGE_NAME={image_name},'
+               '_BRANCH_NAME={branch_name},'
+               '_VERSION={version},'
+               '_BUILD_NUMBER={build_number}'
+               ' --config={cloudbuild_config} .'
+               .format(account=options.gcb_service_account,
+                       project=options.gcb_project,
+                       image_name=service_name,
+                       branch_name=options.git_branch,
+                       version = source_info.summary.version,
+                       build_number = source_info.build_number,
+                       cloudbuild_config=cloudbuild_config))
+
+    logfile = self.get_logfile_path(repository.name + '-gcb-build')
+    labels = {'repository': repository.name}
+    self.metrics.time_call(
+        'DebBuild', labels, self.metrics.default_determine_outcome_labels,
+        check_subprocesses_to_logfile,
+        repository.name + ' deb build', logfile, [command], cwd=repository.git_dir)
+
+
+class BuildDebianFactory(GradleCommandFactory):
+  @staticmethod
+  def add_bom_parser_args(parser, defaults):
+    """Adds publishing arguments of interest to the BOM commands as well."""
+    if hasattr(parser, 'added_debian'):
+      return
+    parser.added_debian = True
+    GradleCommandFactory.add_bom_parser_args(parser, defaults)
+
+  def init_argparser(self, parser, defaults):
+    super(BuildDebianFactory, self).init_argparser(parser, defaults)
+
+    self.add_bom_parser_args(parser, defaults)
+    BranchSourceCodeManager.add_parser_args(parser, defaults)
+    self.add_argument(
+        parser, 'gcb_project', defaults, None,
+        help='The GCP project ID when using the GCP Container Builder.')
+    self.add_argument(
+        parser, 'gcb_service_account', defaults, None,
+        help='Google Service Account when using the GCP Container Builder.')
 
 
 def add_bom_parser_args(parser, defaults):
   """Adds parser arguments pertaining to publishing boms."""
   # These are implemented by the gradle factory, but conceptually
   # for debians, so are exported this way.
-  GradleCommandFactory.add_bom_parser_args(parser, defaults)
+  BuildDebianFactory.add_bom_parser_args(parser, defaults)
 
 
 def register_commands(registry, subparsers, defaults):
-  build_debian_factory = GradleCommandFactory(
+  build_debian_factory = BuildDebianFactory(
       'build_debians', BuildDebianCommand,
       'Build one or more debian packages from the local git repository.',
       BomSourceCodeManager)
