bfb9e271393a7ac9f59ebc769b55394199c38aaa
Michael Plump <plumpy@google.com>
2020-03-06 14:50:17 -0500

    feat(builds): add a script to build containers outside of GCB (#34)

---
+++ build-service-container.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Run this script from the root directory of a spinnaker microservice repository
+# to build a container for that service. As an argument, pass the OS Dockerfile
+# that you would like to build (e.g. Dockerfile.slim or Dockerfile.ubuntu)
+
+if [[ $1 == "" || ! $(basename $1) =~ ^Dockerfile\. ]]; then
+  echo "You must pass a Dockerfile as an argument (e.g. Dockerfile.slim)"
+  exit 1
+fi
+
+if [[ ! -f Dockerfile.compile ]]; then
+  echo "This must be run from within the root directory of a spinnaker "
+  echo "microservice repository."
+  exit 1
+fi
+
+./gradlew clean
+docker build -f Dockerfile.compile -t compile .
+docker run \
+  --user $(id -u):$(id -g) \
+  --mount type=bind,source=$(pwd),target=/workspace \
+  -w /workspace \
+  compile
+docker build -f $1 .
